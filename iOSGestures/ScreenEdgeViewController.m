edf5cdbe2b3fc8e27c6b8bfa5fb75877608a1f2d
Kevin T <kevin.tai.mail@gmail.com>
2018-02-25 16:50:54 -0800

    screen edge view will not stay out

--- iOSGestures/ScreenEdgeViewController.m
+++ iOSGestures/ScreenEdgeViewController.m
@@ -1,125 +1,110 @@
 //
 //  ScreenEdgeViewController.m
-//  iOSGestures
 //
-//  Created by KevinT on 2018-02-25.
-//  Copyright © 2018 KevinT. All rights reserved.
+//  Implemented by KevinT.
+//
+//  Referenced SwipeEdgeGesture by Paul Solt from 3/21/14.
+//  Copyright (c) 2014 Paul Solt. All rights reserved.
 //
 
 #import "ScreenEdgeViewController.h"
 
 @interface ScreenEdgeViewController () <UIGestureRecognizerDelegate> {
   CGFloat _centerX;
-
 }
 
-@property (weak, nonatomic) UIView *panView;
-@property (weak, nonatomic) UIView *edgeView;
-@property (weak, nonatomic) UISwitch *multipleGesturesSwitch;
-//@property (weak, nonatomic) CGPoint originalLocation;
+//@property (weak, nonatomic) IBOutlet UISwitch *multipleGesturesSwitch;
+@property (weak, nonatomic) IBOutlet UIView *edgeView;
 
 @end
 
 @implementation ScreenEdgeViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view.
-//
-//  CGFloat width = 200;
-//  CGFloat height = 200;
-//
-//  CGRect frame = CGRectMake(CGRectGetMidX(self.view.bounds) - width/2, CGRectGetMidY(self.view.bounds) - height/2, width, height);
-//
-//  UIView *view = [[UIView alloc] initWithFrame:frame];
-//  view.userInteractionEnabled = YES;
-//  view.backgroundColor = [UIColor redColor];
-//  [self.view addSubview:view];
-//
-//  UIRotationGestureRecognizer *rotationGesture = [[UIRotationGestureRecognizer alloc] initWithTarget:self action:@selector(viewRotated:)];
-//  [view addGestureRecognizer:rotationGesture];
+- (void)viewDidLoad
+{
+  [super viewDidLoad];
   
+  UIScreenEdgePanGestureRecognizer *leftEdgeGesture = [[UIScreenEdgePanGestureRecognizer alloc] initWithTarget:self action:@selector(handleLeftEdgeGesture:)];
+  leftEdgeGesture.edges = UIRectEdgeLeft;
+  leftEdgeGesture.delegate = self;
+  [self.view addGestureRecognizer:leftEdgeGesture];
   
-  CGFloat width = 200;
-  CGFloat height = 200;
+  UIScreenEdgePanGestureRecognizer *rightEdgeGesture = [[UIScreenEdgePanGestureRecognizer alloc] initWithTarget:self action:@selector(handleRightEdgeGesture:)];
+  rightEdgeGesture.edges = UIRectEdgeRight;
+  rightEdgeGesture.delegate = self;
+  [self.view addGestureRecognizer:rightEdgeGesture];
   
-  CGRect frame = CGRectMake(CGRectGetMaxX(self.view.bounds) - 25, CGRectGetMidY(self.view.bounds) - height/2, width, height);
+//  UIPanGestureRecognizer *panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(handlePanGesture:)];
+//  [self.panView addGestureRecognizer:panGesture];
   
-  UIView *view = [[UIView alloc] initWithFrame:frame];
-  view.userInteractionEnabled = YES;
-  view.backgroundColor = [UIColor greenColor];
-  [self.view addSubview:view];
-  
-  UIScreenEdgePanGestureRecognizer *screenEdgePannedGesture = [[UIScreenEdgePanGestureRecognizer alloc] initWithTarget:self action:@selector(viewScreenEdgePanned:)];
-  screenEdgePannedGesture.edges = UIRectEdgeRight;
-  screenEdgePannedGesture.delegate = self;
-  [self.view addGestureRecognizer:screenEdgePannedGesture];
-  
-  UIPanGestureRecognizer *screenPannedGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(viewPanned:)];
-  [self.view addGestureRecognizer:screenPannedGesture];
-  
-//    _centerX = self.view.bounds.size.width / 2;
-  _centerX = CGRectGetMaxX(self.view.bounds) - 25;
-  
+  // Store the center, so we can animate back to it after a slide
+  _centerX = self.edgeView.center.x;
 }
 
-- (void)viewScreenEdgePanned:(UIScreenEdgePanGestureRecognizer *) sender {
+//- (void)handlePanGesture:(UIPanGestureRecognizer *)gesture {
+//
+//  CGPoint translation = [gesture translationInView:gesture.view];
+//  self.panGestureLabel.text = [NSString stringWithFormat:@"(%0.0f, %0.0f)", translation.x, translation.y];
   
-  UIView *view = [self.view hitTest:[sender locationInView:sender.view] withEvent:nil];
   
-  if (sender.state == UIGestureRecognizerStateBegan || sender.state == UIGestureRecognizerStateChanged) {
-    CGPoint translation = [sender translationInView:sender.view];
+//}
+//- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
+//
+//  // You can customize the way in which gestures can work
+//
+//  // Enabling multiple gestures will allow all of them to work together, otherwise only the topmost view's gestures will work (i.e. PanGesture view on bottom)
+//
+//  return self.multipleGesturesSwitch.on;
+//}
+
+- (void)handleLeftEdgeGesture:(UIScreenEdgePanGestureRecognizer *)gesture {
+  
+  // Get the current view we are touching
+  UIView *view = [self.view hitTest:[gesture locationInView:gesture.view] withEvent:nil];
+  
+  if(UIGestureRecognizerStateBegan == gesture.state ||
+     UIGestureRecognizerStateChanged == gesture.state) {
+    CGPoint translation = [gesture translationInView:gesture.view];
+//    self.leftDistanceLabel.text = [NSString stringWithFormat:@"%0.1f", translation.x];
     
-    view.center = CGPointMake(_centerX, view.center.y);
-//    view.center = CGPointMake(_centerX + translation.x, view.center.y);
-  } else {
-    [UIView animateWithDuration:.3 animations:^{view.center = CGPointMake(_centerX, view.center.y);}];
+    view.center = CGPointMake(_centerX + translation.x, view.center.y);
+  } else {  // cancel, fail, or ended
+    // reset
+//    self.leftDistanceLabel.text = @"0.0";
+    
+    // Animate back to center x
+    [UIView animateWithDuration:.3 animations:^{
+      view.center = CGPointMake(_centerX, view.center.y);
+      
+    }];
+  }
+}
+
+- (void)handleRightEdgeGesture:(UIScreenEdgePanGestureRecognizer *)gesture {
+  
+  // Get the current view we are touching
+  UIView *view = [self.view hitTest:[gesture locationInView:gesture.view] withEvent:nil];
+  
+  if(UIGestureRecognizerStateBegan == gesture.state ||
+     UIGestureRecognizerStateChanged == gesture.state) {
+    CGPoint translation = [gesture translationInView:gesture.view];
+//    self.rightDistanceLabel.text = [NSString stringWithFormat:@"%0.1f", translation.x];
+    
+    view.center = CGPointMake(_centerX + translation.x, view.center.y);
+    
+  } else {  // cancel, fail, or ended
+    // reset
+//    self.rightDistanceLabel.text = @"0.0";
+    
+    // Animate back to center x
+    [UIView animateWithDuration:.3 animations:^{
+      view.center = CGPointMake(_centerX, view.center.y);
+      
+    }];
+    
   }
   
-//  CGFloat scale = sender.scale;
-//  sender.view.transform = CGAffineTransformMakeScale(scale, scale);
-//
-//  scale = 1.0;
 }
 
-- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer {
-  
-  // You can customize the way in which gestures can work
-  
-  // Enabling multiple gestures will allow all of them to work together, otherwise only the topmost view's gestures will work (i.e. PanGesture view on bottom)
-  
-  return self.multipleGesturesSwitch.on;
-}
+@end
 
-- (void)viewPanned:(UIPanGestureRecognizer *) sender {
-  
-  [UIView animateWithDuration:0.5 animations:^{
-    sender.view.frame = CGRectOffset(sender.view.frame, -5.0, 0.0);
-  }];
-  
-//  CGPoint translation = [sender translationInView:sender.view];
-  
-//  CGPoint translationInView = [sender translationInView:self.view];
-//  CGPoint oldCenter = sender.view.center;
-//  CGPoint newCenter = CGPointMake(CGRectGetMaxX(self.view.bounds) - 25, CGRectGetMidY(self.view.bounds) - 100);
-//  sender.view.center = newCenter;
-//  [sender setTranslation:CGPointZero inView:self.view];
-  //  CGFloat scale = sender.scale;
-  //  sender.view.transform = CGAffineTransformMakeScale(scale, scale);
-  //
-  //  scale = 1.0;
-}
-
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
-   @end
-   
